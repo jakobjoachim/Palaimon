02bc41c1e43c1d812d8f3e3635c2c4697ee6b631
Finn Masurat <finn.masurat@hotmail.de>
2017-06-02 11:32:47 +0200

    3.5 und 4.1

--- scripte/server.sh
+++ scripte/server.sh
@@ -11,3 +11,17 @@ iptables -A OUTPUT -p tcp --dport ssh -j ACCEPT # allow new outgoing ssh connect
 iptables -P FORWARD DROP # dont allow any forwarding connections
 iptables -P INPUT DROP # dont allow any other incoming connections
 iptables -P OUTPUT DROP # dont allow any other outgoing connections
+
+# ONLY Server 1
+iptables -A INPUT -p tcp --dport 465 -j ACCEPT # allow new incoming secure smtp
+iptables -A OUTPUT -p tcp --dport 465 -j ACCEPT # allow new outgoing secure smtp
+iptables -A INPUT -p tcp --dport 993 -j ACCEPT # allow new incoming secure imap
+
+# ONLY Server 2
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT # allow new incoming secure http
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT # allow new incoming naughty http
+
+# ONLY Server 3
+iptables -A INPUT -p tcp --dport 3128 -j ACCEPT # allow new incoming proxy connections
+iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT # allow new outgoing secure http
+iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT # allow new outgoing naughty http
